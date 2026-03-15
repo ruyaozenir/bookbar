3df2ba3854caf9093db430084dba86ad475cec4c
Rüya Özenir <164229173+ruyaozenir@users.noreply.github.com>
2026-03-15 17:14:40 +0300

    Update API base URL to production endpoint

--- bookshelf.js
+++ bookshelf.js
@@ -15,7 +15,7 @@ function debugError(...args) {
 }
 
 // API Helper Functions
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://bookbar-ur55.onrender.com/api';
 
 async function apiRequest(endpoint, options = {}) {
     const token = localStorage.getItem('bookbar_token');
